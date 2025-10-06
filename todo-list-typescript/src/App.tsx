708a58ce9e35d32f1fb9942bda784902a6887a6c
Cristiane Abreu <bjwmcd@yahoo.com>
2025-10-06 12:40:23 -0400

    update

--- todo-list-typescript/src/App.tsx
+++ todo-list-typescript/src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css';
+import TodoList from './components/TodoList';
 
 function App(){
 
   const fullName: string = "Coding Temple"
   const year: number = 2024
-  const isThisBootCampFun: boolean = true;  
+  const isThisBootCampFun: boolean = true;
   
-  /* added code starts */
   type Person = {
     name: string;
     age: number;
@@ -16,21 +16,48 @@ function App(){
     name: "Alex",
     age: 26,
   };
-  /* added code stops */
+  
+  const fruits: string[] = ["Bananas", "Orange", "Grapes"];
+  console.log("Fruits ", fruits); 
 
-  //Render the typed data to the screen
+  /* added code starts here */
+  const shapeShifter: any = {
+    name: 24,
+    age: 'Alex'
+  }
+
+  const printToConsole = (value: string): void => {
+    console.log(value);
+  }
+
+  const cat: null = null;
+  const dog: undefined = undefined;
+  console.log('cat ',cat);
+  console.log('dog',dog);
+  /* added code stops here */
+
   return (
     <div>
+      <TodoList/>
       <p>Full Name: {fullName}</p>
       <p>Year: {year}</p>
       <p>Is this Bootcamp fun: {isThisBootCampFun ? 'Yes' : 'No'}</p>
-      {/* added code starts here */}
       <p>name: {person1.name}</p>
       <p>age: {person1.age}</p>
-      {/* added code stops here */}
+      
+      <p>Fruit 1: {fruits[0]}</p>
+      <p>Fruit 2: {fruits[1]}</p>
+      <p>Fruit 3: {fruits[2]}</p>      
+
+      {/* added code starts */}
+      <button onClick={() => printToConsole('Hello, TypeScript!')}>Click me</button>
+      <p>name: {shapeShifter.name}</p>
+      <p>age: {shapeShifter.age}</p>
+      <p>cat: {cat}</p>
+      <p>dog: {dog}</p>
+      {/* added code stops */}
     </div>
   );
 }
 
 export default App;
-
